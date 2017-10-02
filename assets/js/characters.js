1f9c0779d5e4ec87506d59f1341553133955acd4
AndrewD14 <Leingod2005@gmail.com>
2017-10-02 15:51:45 -0500

    Created the basic character commands and tested, have some general rules set as arrays, start of the character list

---
+++ assets/js/characters.js
@@ -0,0 +1,12 @@
+var characterOptions = [
+	{
+		hp: 0,
+		hpMultiplier: 0,
+		level: 0,
+		attackPower: 0,
+		attackPowerMultiplier: 0,
+		blockStr: 0,
+		blockStrMultiplier: 0,
+		expWorth: 0
+	}
+]
---
+++ assets/js/game-rules.js
@@ -0,0 +1,5 @@
+var exp_chart = [100, 300, 500, 1000, 1500, 2000, 3000, 4000, 5000];
+
+var max_healing_carry_small = [3, 3, 3, 4, 4, 4, 5, 5, 5, 6];
+var max_healing_carry_med = [0, 0, 0, 1, 1, 1, 3, 3, 3, 4];
+var max_healing_carry_lrg = [0, 0, 0, 0, 0, 2, 2, 2, 3, 3];
