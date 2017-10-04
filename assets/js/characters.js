85ebfdd8ad739c1037526645b640fb3c28298a03
AndrewD14 <Leingod2005@gmail.com>
2017-10-03 23:56:17 -0500

    Adjusted the stats to get the scenarios to work.

--- assets/js/characters.js
+++ assets/js/characters.js
@@ -1,33 +1,33 @@
 var characterOptions = [
 	{
 		name: "Luke Skywalker",
-		hp: 80,
-		attackPower: 10,
-		counterAttackPower: 10,
+		hp: 200,
+		attackPower: 7,
+		counterAttackPower: 40,
 		portrait: "assets/images/luke-skywalker.jpg"
 	},
 
 	{
 		name: "Darth Maul",
-		hp: 60,
+		hp: 100,
 		attackPower: 20,
-		counterAttackPower: 20,
+		counterAttackPower: 30,
 		portrait: "assets/images/darth-maul.jpg"
 	},
 
 	{
 		name: "Darth Vader",
-		hp: 60,
-		attackPower: 30,
-		counterAttackPower: 30,
+		hp: 180,
+		attackPower: 12,
+		counterAttackPower: 20,
 		portrait: "assets/images/darth-vader.jpg"
 	},
 
 	{
 		name: "Yoda",
-		hp: 50,
-		attackPower: 10,
-		counterAttackPower: 40,
+		hp: 170,
+		attackPower: 15,
+		counterAttackPower: 10,
 		portrait: "assets/images/yoda.jpg"
 	}
 ]
